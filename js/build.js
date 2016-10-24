cc2661a50ec1facd4f82a164835d555b28d32209
Tony Wu <tonytlwu@gmail.com>
2016-10-24 10:15:31 +0100

    Adds swipe-to-save prototype

--- js/build.js
+++ js/build.js
@@ -1,12 +1,23 @@
 $('.linked[data-list-item-id]').click(function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var data = Fliplet.Widget.getData($(this).parents('[data-list-id]').data('list-id'));
+  var data = Fliplet.Widget.getData($(this).parents('[data-list-id]').data('list-id'));
 
-    var itemData = _.find(data.items,{id: $(this).data('list-item-id')});
+  var itemData = _.find(data.items,{id: $(this).data('list-item-id')});
 
-    if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
-        Fliplet.Navigate.to(itemData.linkAction);
-    }
+  if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+    Fliplet.Navigate.to(itemData.linkAction);
+  }
 });
 
+$('[data-list-id]').each(function(){
+  var data = Fliplet.Widget.getData($(this).data('list-id'));
+  // if (data.swipeToSave) {
+  if (true) {
+    window.ui = window.ui || {};
+    window.ui['swipeSavedList' + $(this).data('list-id')] = new SwipeSaveList(this, {
+      // savedListLabel: data.swipeToSaveLabel || 'My list'
+      savedListLabel: 'My list'
+    });
+  }
+});
